f240349e7950ffff849bbc08037fdd27265f252c
moha11 <mohamed.tanger2001@gmail.com>
2025-02-15 18:19:52 +0100

    añadido servidor express para desplegarlo en Render

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url'; // Necesario para __dirname
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sirve los archivos estáticos desde la carpeta dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Redirige todas las rutas a index.html para manejar el enrutamiento en React
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Arranca el servidor
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
