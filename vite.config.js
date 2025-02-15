89f50684232494fcf9103d8202b91e54e3dbf754
moha11 <mohamed.tanger2001@gmail.com>
2025-02-15 17:47:18 +0100

    Agregado polyfills para Node en producción

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       stream: 'stream-browserify',
       crypto: 'crypto-browserify',
+      events: 'events',
     },
   },
   optimizeDeps: {
@@ -25,5 +28,15 @@ export default defineConfig({
         NodeModulesPolyfillPlugin(),
       ],
     },
-  }
+  },
+  
+  build: {
+    rollupOptions: {
+      // Aquí es donde metemos el plugin para producción:
+      plugins: [
+        rollupNodePolyFill(),
+      ],
+    },
+  },
+
 })
